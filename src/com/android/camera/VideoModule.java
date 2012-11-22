3e12fa95f6ea3d749668a08410fc964bd03f0765
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-11-22 15:26:13 +0700

    VideoModule: check for videosnapshot support in onSingleTapUp
    onSingleTapUp is now triggered through PreviewGestures instead of
    ActivityBase, so the check in initializeVideoSnapshot() is no
    longer enough to prevent triggering video snapshot on unsupported
    devices.
    
    Change-Id: Iab0b4b3d0cd9b0ea7278eceee3ae58419f5df100

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -2453,7 +2453,8 @@ public class VideoModule implements CameraModule,
             return;
         }
 
-        if (mPaused || mSnapshotInProgress || effectsActive()) {
+        if (!mPaused || mSnapshotInProgress || effectsActive()
+                || !Util.isVideoSnapshotSupported(mParameters)) {
             return;
         }
 
